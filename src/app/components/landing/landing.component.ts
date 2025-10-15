ea0ec816a21bae1a992140a2f8c108e3db688971
Bikiega Faril <adamafarilb@gmail.com>
2025-10-15 15:43:29 +0000

    modification pour l'affichage des icones

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,140 +1,3 @@
-// import { Component, OnInit } from '@angular/core';
-// import { CommonModule } from '@angular/common';
-// import { RouterModule } from '@angular/router';
-// import { FormsModule } from '@angular/forms';
-// import { HttpClientModule } from '@angular/common/http';
-// import { LanguageSwitcherComponent } from '../shared/language-switcher/language-switcher.component';
-// import { I18nService } from '../../services/i18n.service';
-// import { Nav, Hero, Services, Benefits, Statistiques, CTA, Contact, Footer, Common } from '../../models/landing.model';
-// import { LandingService } from '../../services/landing.service';
-
-// @Component({
-//   selector: 'app-landing',
-//   standalone: true,
-//   imports: [CommonModule, RouterModule, FormsModule, HttpClientModule, LanguageSwitcherComponent],
-//   templateUrl: './landing.component.html',
-//   styleUrls: ['./landing.component.css']
-// })
-// export class LandingComponent implements OnInit {
-//   translations: any = {};
-  
-
-//   // Données depuis Strapi
-//   nav?: Nav;
-//   hero?: Hero;
-//   services?: Services;
-//   benefits?: Benefits;
-//   stats?: Statistiques;
-//   cta?: CTA;
-//   contact?: Contact;
-//   footer?: Footer;
-//   common?: Common;
-
-//   contactForm = {
-//     firstName: '',
-//     lastName: '',
-//     email: '',
-//     profile: '',
-//     message: ''
-//   };
-
-//   constructor(
-//     private i18nService: I18nService,
-//     private landingService: LandingService
-//   ) {}
-
-//   ngOnInit() {
-//     this.loadTranslations();
-
-//     // Recharge à chaque changement de langue
-//     this.i18nService.currentLocale$.subscribe(() => {
-//       this.loadTranslations();
-//       this.loadContent(); // recharge aussi depuis Strapi
-//     });
-
-//     this.loadContent();
-//   }
-//   private loadTranslations() {
-//     const currentLocale = this.i18nService.getCurrentLocale();
-//     this.i18nService.loadTranslations(currentLocale).subscribe(translations => {
-//       this.translations = translations;
-//     });
-//   }
-
-//   private loadContent() {
-//     this.landingService.getNav().subscribe(res => {
-//   if (res.data.length > 0) {
-//     this.nav = res.data[0];  // déjà compatible
-//   }
-// });
-
-// this.landingService.getHero().subscribe(res => {
-//   if (res.data.length > 0) {
-//     this.hero = res.data[0];
-//   }
-// });
-
-// this.landingService.getServices().subscribe(res => {
-//   if (res.data.length > 0) {
-//     this.services = res.data[0]; // ⚡ pas de .attributes
-//   }
-// });
-
-//     this.landingService.getBenefits().subscribe(res => {
-//       if (res.data.length > 0) {
-//         this.benefits = res.data[0];
-//       }
-//     });
-
-//     this.landingService.getStats().subscribe(res => {
-//       if (res.data.length > 0) {
-//         this.stats = res.data[0];
-//       }
-//     });
-
-//     this.landingService.getCTA().subscribe(res => {
-//       if (res.data.length > 0) {
-//         this.cta = res.data[0];
-//       }
-//     });
-
-//     this.landingService.getContact().subscribe(res => {
-//       if (res.data.length > 0) {
-//         this.contact = res.data[0];
-//       }
-//     });
-
-//     this.landingService.getFooter().subscribe(res => {
-//       if (res.data.length > 0) {
-//         this.footer = res.data[0];
-//       }
-//     });
-
-//     this.landingService.getCommon().subscribe(res => {
-//       if (res.data.length > 0) {
-//         this.common = res.data[0];
-//       }
-//     });
-//   }
-
-//   t(key: string): string {
-//     return this.i18nService.translate(key, this.translations);
-//   }
-
-//   scrollToSection(sectionId: string) {
-//     const element = document.getElementById(sectionId);
-//     if (element) {
-//       element.scrollIntoView({ behavior: 'smooth' });
-//     }
-//   }
-
-//   submitContactForm() {
-//     console.log('Formulaire de contact soumis:', this.contactForm);
-//     // TODO: Envoyer à Strapi via POST si besoin
-//   }
-// }
-
-
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
@@ -200,10 +63,12 @@ export class LandingComponent implements OnInit {
 
     this.landingService.getServices(locale).subscribe(data => {
       this.services = data || undefined;
+      console.log('Services loaded:', this.services); // Debug
     });
 
     this.landingService.getBenefits(locale).subscribe(data => {
       this.benefits = data || undefined;
+      console.log('Benefits loaded:', this.benefits); // Debug
     });
 
     this.landingService.getStats(locale).subscribe(data => {
@@ -216,6 +81,7 @@ export class LandingComponent implements OnInit {
 
     this.landingService.getContact(locale).subscribe(data => {
       this.contact = data || undefined;
+      console.log('Contact loaded:', this.contact); // Debug
     });
 
     this.landingService.getFooter(locale).subscribe(data => {
@@ -227,16 +93,68 @@ export class LandingComponent implements OnInit {
     });
   }
 
+  /**
+   * Retourne la classe CSS pour l'icône du service en fonction de l'index
+   */
+  getServiceIconClass(index: number): string {
+    const classes = ['debtor', 'bailiff', 'lawyer', 'creditor', 'cedant'];
+    return classes[index] || 'debtor';
+  }
+
+  /**
+   * Scroll smooth vers une section
+   */
   scrollToSection(sectionId: string) {
     const element = document.getElementById(sectionId);
     if (element) {
-      element.scrollIntoView({ behavior: 'smooth' });
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
     }
   }
 
+  /**
+   * Soumet le formulaire de contact
+   */
   submitContactForm() {
     console.log('Formulaire de contact soumis:', this.contactForm);
-    // TODO: Envoyer à Strapi via POST si besoin
-    // this.http.post(`${apiUrl}/contacts`, this.contactForm).subscribe(...)
+    
+    // Validation basique
+    if (!this.contactForm.firstName || !this.contactForm.lastName || 
+        !this.contactForm.email || !this.contactForm.message) {
+      alert('Veuillez remplir tous les champs obligatoires');
+      return;
+    }
+
+    // TODO: Envoyer à Strapi ou à un service de messagerie
+    // Exemple avec Strapi:
+    // this.http.post(`${environment.apiUrl}/contact-submissions`, {
+    //   data: this.contactForm
+    // }).subscribe({
+    //   next: (response) => {
+    //     console.log('Message envoyé avec succès', response);
+    //     this.resetContactForm();
+    //     alert('Votre message a été envoyé avec succès !');
+    //   },
+    //   error: (error) => {
+    //     console.error('Erreur lors de l\'envoi', error);
+    //     alert('Une erreur est survenue. Veuillez réessayer.');
+    //   }
+    // });
+
+    // Pour le moment, juste un reset et un message
+    alert('Votre message a été envoyé avec succès !');
+    this.resetContactForm();
+  }
+
+  /**
+   * Réinitialise le formulaire de contact
+   */
+  private resetContactForm() {
+    this.contactForm = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      profile: '',
+      message: ''
+    };
   }
 }
