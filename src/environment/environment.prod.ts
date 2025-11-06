013233bcd5d094b4560744c450758d070cb9a916
ckonate <constant.konate@sertia.fr>
2025-11-06 14:23:48 +0100

    deployment test

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://aden-strapi.onrender.com/api',
-  baseUrl: 'https://aden-bff-3acv.onrender.com/api',
+  baseUrl: 'https://aden-bff-3acv.onrender.com/api'
 };
