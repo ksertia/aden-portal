99d9c6e79162260d338b92c1750684827830d261
ckonate <constant.konate@sertia.fr>
2025-11-06 10:54:00 +0100

    merge from deploy

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  //apiUrl: 'http://localhost:1337/api',
-  //baseUrl: 'http://localhost:3000/api',
+  apiUrl: 'http://localhost:1337/api',
+  baseUrl: 'http://localhost:3000/api',
 
-  apiUrl: 'https://aden-strapi.onrender.com/api',
-  baseUrl: 'https://aden-bff-3acv.onrender.com/api',
+  //apiUrl: 'https://aden-strapi.onrender.com/api',
+  //baseUrl: 'https://aden-bff-3acv.onrender.com/api',
 };
