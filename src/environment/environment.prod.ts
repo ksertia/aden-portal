f016ce77cf491b63c123b324dc1b2421d61995cc
contactsertia <contact@sertia.fr>
2025-12-12 20:49:35 +0100

    production urls

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,5 +1,8 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://aden-strapi.onrender.com/api',
-  baseUrl: 'https://aden-bff-3acv.onrender.com/api'
+ //apiUrl: 'https://aden-strapi.onrender.com/api',
+ //baseUrl: 'https://aden-bff-3acv.onrender.com/api'
+ apiUrl: 'http://213.32.120.11:1337/api',
+ baseUrl: 'http://213.32.120.11:3003/api'
+
 };
